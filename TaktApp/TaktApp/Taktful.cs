4797539f5ae45445d5109b749854e26083a82b4f
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-07-11 10:45:53 -0600

    removing Json.Net reference in favour of string building

--- TaktApp/TaktApp/Taktful.cs
+++ TaktApp/TaktApp/Taktful.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace TaktApp
 {
@@ -12,14 +10,7 @@ namespace TaktApp
     {
         public static async Task<HttpStatusCode> Tick(string project, string feature, string type, DateTime timestamp)
         {
-            var data = new Dictionary<string, string>
-                {
-                    {"project", project},
-                    {"name", feature},
-                    {"type", type},
-                    {"at", timestamp.ToString("yyyyMMddHHmmss")},
-                };
-            var content = await JsonConvert.SerializeObjectAsync(data);
+            var content = string.Format("{{\"project\":\"{0}\",\"name\":\"{1}\",\"type\":\"{2}\",\"at\":\"{3}\"}}", project, feature, type, timestamp.ToString("yyyyMMddHHmmss"));            
             var url = String.Format("http://taktapp.herokuapp.com/api/{0}/events", project);
             var result = await new HttpClient().PostAsync(url, new StringContent(content, Encoding.UTF8, "application/json"));            
             return result.StatusCode;
