ce340114e014b27403f3fc4c6ce764a17a3eece3
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-07-10 22:43:38 -0600

    initial revision w/async event poster

---
+++ TaktApp/TaktApp/Taktful.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TaktApp
+{
+    public class Taktful
+    {
+        public static async Task<HttpStatusCode> Tick(string project, string feature, string type, DateTime timestamp)
+        {
+            var data = new Dictionary<string, string>
+                {
+                    {"project", project},
+                    {"name", feature},
+                    {"type", type},
+                    {"at", timestamp.ToString("yyyyMMddHHmmss")},
+                };
+            var content = await JsonConvert.SerializeObjectAsync(data);
+            var url = String.Format("http://taktapp.herokuapp.com/api/{0}/events", project);
+            var result = await new HttpClient().PostAsync(url, new StringContent(content, Encoding.UTF8, "application/json"));            
+            return result.StatusCode;
+        }
+    }
+}
---
+++ TaktApp/Tests/TaktfulTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+using TaktApp;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TaktfulTest
+    {
+        [Test]
+        public async void ShouldSendTickEvent()
+        {
+            Assert.AreEqual(HttpStatusCode.OK, await Taktful.Tick("project", "feature", "tick", DateTime.Now));
+        }
+    }
+}
