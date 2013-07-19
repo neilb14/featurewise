9b7fdec5d66156d1e49d0b62f45ee03c83eb95da
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-07-18 22:46:41 -0600

    added workbench and ability to add an event manually

---
+++ src/FWServices/Controllers/WorkbenchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+using GF.FeatureWise.Services.Models;
+using GF.FeatureWise.Services.Repositories;
+
+namespace GF.FeatureWise.Services.Controllers
+{
+    public class WorkbenchController : Controller
+    {
+        private readonly IUserEventRepository repository;
+
+        public WorkbenchController() :this(new UserEventRepository(new ApiDataContext()))
+        {
+        }
+
+        public WorkbenchController(IUserEventRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name, string type)
+        {
+            repository.Add(new UserEvent
+                {
+                    Id = Guid.NewGuid(),
+                    Feature = name,
+                    Type = type,
+                    At = DateTime.UtcNow
+                });
+            return new RedirectResult("/Workbench");
+        }
+    }
+}
---
+++ src/Tests/Controllers/WorkbenchControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using GF.FeatureWise.Services.Controllers;
+using GF.FeatureWise.Services.Models;
+using Moq;
+using Xunit;
+
+namespace Tests.Controllers
+{
+    public class WorkbenchControllerTest
+    {
+        private readonly WorkbenchController controller;
+        private Mock<IUserEventRepository> repository;
+
+        public WorkbenchControllerTest()
+        {
+            repository = new Mock<IUserEventRepository>();
+            controller = new WorkbenchController(repository.Object);
+        }
+
+        [Fact]
+        public void ShouldPostASingleEventToWorkbench()
+        {
+            repository.Setup(r => r.Add(It.IsAny<UserEvent>())).Returns(new UserEvent());
+            var result = controller.Index("Moose", "Tick") as RedirectResult;
+            repository.VerifyAll();
+            Assert.NotNull(result);
+            Assert.Equal("/Workbench", result.Url);
+        }
+    }
+}
