0c60e05c4178e223f7b7bac5ac85f97cac67e974
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-09-09 22:40:47 -0600

    adding histogram chart

--- src/FWServices/Controllers/HistogramDataController.cs
+++ src/FWServices/Controllers/HistogramDataController.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 using GF.FeatureWise.Services.Models;
+using GF.FeatureWise.Services.Repositories;
 
 namespace GF.FeatureWise.Services.Controllers
 {
     public class HistogramDataController : ApiController
     {
         private readonly IHistogramRepository histogramRepository;
+
+        public HistogramDataController() : this(new HistogramRepository(new ApiDataContext()))
+        {
+        }
 
         public HistogramDataController(IHistogramRepository histogramRepository)
         {
