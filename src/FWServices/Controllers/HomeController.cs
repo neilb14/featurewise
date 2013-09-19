c36bb7ae02c152021505c9086d60cb89d9e7f389
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-09-18 22:07:57 -0600

    added sparklines jquery library plus fixed home index to list all features

--- src/FWServices/Controllers/HomeController.cs
+++ src/FWServices/Controllers/HomeController.cs
@@ -1,13 +1,39 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using GF.FeatureWise.Services.Models;
+using GF.FeatureWise.Services.Repositories;
 
 namespace GF.FeatureWise.Services.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITimeSeriesRepository repository;
+
+        public HomeController(ITimeSeriesRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public HomeController() : this(new TimeSeriesRepository(new ApiDataContext()))
+        {
+        }
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
+            ViewBag.Title = "FeatureWise Home";
+            var results = repository.GetAll();
+            var map = new Dictionary<string, List<int>>();            
+            foreach (var result in results)
+            {
+                List<int> data;
+                if (!map.TryGetValue(result.Feature, out data))
+                {
+                    data = new List<int>();
+                    map.Add(result.Feature, data);
+                }
+                data.Add(result.Ticks + result.Starts);                
+            }
+            ViewBag.Features = map;
             return View();
         }
 
---
+++ src/Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using GF.FeatureWise.Services.Controllers;
+using GF.FeatureWise.Services.Models;
+using Moq;
+using Xunit;
+
+namespace Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        private readonly Mock<ITimeSeriesRepository> timeSeriesRepository;
+        private readonly HomeController controller;
+
+        public HomeControllerTest()
+        {
+            timeSeriesRepository = new Mock<ITimeSeriesRepository>();
+            controller = new HomeController(timeSeriesRepository.Object);
+        }
+
+        [Fact]
+        public void GetIndex_ShouldBuildListOfFeaturesWithSparklines()
+        {
+            timeSeriesRepository.Setup(r => r.GetAll()).Returns(new[]
+                {
+                    new TimeSeries {Feature = "Moose", Ticks = 10, Starts = 30, Year = 2013, Month = 9, Day = 18},
+                    new TimeSeries {Feature = "Moose", Ticks = 11, Starts = 35, Year = 2013, Month = 9, Day = 19},
+                    new TimeSeries {Feature = "Moose", Ticks = 12, Starts = 30, Year = 2013, Month = 9, Day = 20}
+                });
+            controller.Index();
+            Dictionary<string, List<int>> map = controller.ViewBag.Features;            
+            Assert.Equal(1, map.Count);
+            Assert.True(map.ContainsKey("Moose"));
+            timeSeriesRepository.VerifyAll();
+        }
+    }
+}
