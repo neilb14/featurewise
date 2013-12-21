6cf1f0d71a4263d5455a5e257baf8fca761a9087
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-12-21 13:03:08 -0700

    fixed usage generation script to properly alter durations throughout the year of simulation

--- scripts/generate_usage.py
+++ scripts/generate_usage.py
@@ -3,15 +3,17 @@ import datetime
 import random
 from datetime import timedelta
 
-def linear_increase(now, min_duration, max_duration):
-	percent = now/1300000.0
-	result = min_duration + percent*max_duration
-	return result
+def working_hours(current_time):
+	return current_time.hour < 17 and current_time.hour > 8
+
+def linear_increase(now, min_duration, max_duration):	
+	end_of_time = 60*60*24*365
+	time_left = end_of_time - now	
+	return min_duration + max_duration *  time_left / end_of_time
 
 def linear_decrease(now, min_duration, max_duration):
-	percent = (1300000.0-now)/1300000.0
-	result = min_duration + percent*max_duration
-	return result
+	end_of_time = 60*60*24*365	
+	return min_duration + max_duration *  now / end_of_time
 
 random_duration = lambda x,min_duration,max_duration : random.gauss((max_duration-min_duration)/2, (max_duration-min_duration)/(6))
 increasing_duration = lambda x,min_duration,max_duration : linear_increase(x,min_duration,max_duration)
@@ -24,11 +26,9 @@ events = []
 def user(env, name, duration, min_duration, max_duration):
 	yield env.timeout(30)
 	current_time = start
-	print("Current Time: %s" % current_time.hour)
 	while current_time < end_time:
 		current_time = start + timedelta(seconds=env.now)
-		if(current_time.hour < 17 and current_time.hour > 8):
-			print("[%d] %s event at %s" % (env.now, name, current_time))
+		if(working_hours(current_time)):
 			events.append({'name':name,'at':current_time, 'type':'tick'})
 			duration_in_s = duration(env.now, min_duration, max_duration)
 			while(duration_in_s < 0):
@@ -39,31 +39,13 @@ def user(env, name, duration, min_duration, max_duration):
 
 env = simpy.Environment()
 #env.process(user(env, 'rhino', decreasing_duration, 30, 1200))
-env.process(user(env, 'cheetah', increasing_duration, 600, 2000))
+env.process(user(env, 'cheetah', increasing_duration, 60*2, 60*60*4))
 env.process(user(env, 'moose', random_duration, 200, 6000))
-env.process(user(env, 'lion', increasing_duration, 400, 3000))
+env.process(user(env, 'lion', increasing_duration, 60*5, 60*60*1))
 #env.process(user(env, 'mouse', decreasing_duration, 50, 1000))
-env.process(user(env, 'hippo', increasing_duration, 900, 4000))
+env.process(user(env, 'hippo', increasing_duration, 60*60*1, 60*60*6))
 env.process(user(env, 'giraffe', random_duration, 1000,10000))
 env.run()
-
-print("Total Events: %d" % len(events))
-report = {}
-for e in events:
-	key = "%d%d%d" % (e['at'].year, e['at'].month, e['at'].day)
-	name = e['name']
-	if key not in report:
-		report[key] = {}
-	if name not in report[key]:
-		report[key][name] = 0
-	report[key][name] += 1
-
-for k in sorted(report.keys()):
-	print("%s" % k)
-	for name in sorted(report[k].keys()):
-		print("  %s | %d" % (name, report[k][name]))
-
-print("Data has been written to: data.csv")		
 
 str = ","
 fieldNames = ['feature', 'type','at']
@@ -75,3 +57,4 @@ with open('data.csv', 'w') as f:
 		fields.append(row['type'])
 		fields.append(row['at'].strftime("%Y-%m-%dT%H:%M:%S"))
 		f.write(str.join(fields) + "\n")			
+print("Data has been written to: data.csv")		
