f38647c448910c3a5a6214135db49da212f0804e
Neil Bourgeois <neil.bourgeois@gmail.com>
2013-12-23 22:16:20 -0700

    added features that decrease in activity

--- scripts/generate_usage.py
+++ scripts/generate_usage.py
@@ -3,23 +3,23 @@ import datetime
 import random
 from datetime import timedelta
 
+end_of_time = 60*60*24*365
+
 def working_hours(current_time):
 	return current_time.hour < 17 and current_time.hour > 8
 
-def linear_increase(now, min_duration, max_duration):	
-	end_of_time = 60*60*24*365
+def linear_increase(now, min_duration, max_duration):		
 	time_left = end_of_time - now	
 	return min_duration + max_duration *  time_left / end_of_time
 
 def linear_decrease(now, min_duration, max_duration):
-	end_of_time = 60*60*24*365	
 	return min_duration + max_duration *  now / end_of_time
 
 random_duration = lambda x,min_duration,max_duration : random.gauss((max_duration-min_duration)/2, (max_duration-min_duration)/(6))
 increasing_duration = lambda x,min_duration,max_duration : linear_increase(x,min_duration,max_duration)
 decreasing_duration = lambda x,min_duration,max_duration : linear_decrease(x,min_duration,max_duration)
 
-start = datetime.datetime.now() - timedelta(weeks=52)
+start = datetime.datetime.now() - timedelta(seconds=end_of_time)
 end_time = datetime.datetime.now()
 events = []
 
@@ -38,11 +38,11 @@ def user(env, name, duration, min_duration, max_duration):
 			yield env.timeout(15*60)
 
 env = simpy.Environment()
-#env.process(user(env, 'rhino', decreasing_duration, 30, 1200))
-env.process(user(env, 'cheetah', increasing_duration, 60*2, 60*60*4))
-env.process(user(env, 'moose', random_duration, 200, 6000))
+env.process(user(env, 'rhino', decreasing_duration, 60*10, 60*60*2))
+env.process(user(env, 'cheetah', increasing_duration, 60*2, 60*45))
+env.process(user(env, 'moose', random_duration, 50, 6000))
 env.process(user(env, 'lion', increasing_duration, 60*5, 60*60*1))
-#env.process(user(env, 'mouse', decreasing_duration, 50, 1000))
+env.process(user(env, 'mouse', decreasing_duration, 60*15, 60*60*2))
 env.process(user(env, 'hippo', increasing_duration, 60*60*1, 60*60*6))
 env.process(user(env, 'giraffe', random_duration, 1000,10000))
 env.run()
