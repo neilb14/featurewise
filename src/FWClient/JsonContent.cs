0ed18bdd046c5725f66031f3dbaecfa4d288256a
Neil Bourgeois <neil.bourgeois@gmail.com>
2014-12-30 10:29:17 -0700

    changed JSON date strings to use ISO 8601 format

--- src/FWClient/JsonContent.cs
+++ src/FWClient/JsonContent.cs
@@ -6,7 +6,7 @@ namespace GF.FeatureWise.Client
     {
         public static string Build(Guid id, string feature, string type, DateTime at)
         {
-            return string.Format("{{\"Id\":\"{0}\",\"Feature\":\"{1}\",\"Type\":\"{2}\",\"At\":\"{3}\"}}", id,feature, type, at.ToString("yyyyMMddHHmmss"));
+            return string.Format("{{\"Id\":\"{0}\",\"Feature\":\"{1}\",\"Type\":\"{2}\",\"At\":\"{3}\"}}", id,feature, type, at.ToString("o"));
         }
     }
 }
--- src/Tests/Client/JsonContentTest.cs
+++ src/Tests/Client/JsonContentTest.cs
@@ -9,8 +9,8 @@ namespace Tests.Client
         [Fact]
         public void ShouldGenerateJsonForPosting()
         {
-            var at = new DateTime(2013,9,5,8,0,30);
-            Assert.Equal("{\"Id\":\"10fb9c71-a314-41ea-9dca-369824d14dad\",\"Feature\":\"moose\",\"Type\":\"tick\",\"At\":\"20130905080030\"}",JsonContent.Build(Guid.Parse("10fb9c71-a314-41ea-9dca-369824d14dad"), "moose", "tick", at));
+            var at = new DateTime(2013,9,5,8,15,30);
+            Assert.Equal("{\"Id\":\"10fb9c71-a314-41ea-9dca-369824d14dad\",\"Feature\":\"moose\",\"Type\":\"tick\",\"At\":\"2013-0905-T08:15:30\"}",JsonContent.Build(Guid.Parse("10fb9c71-a314-41ea-9dca-369824d14dad"), "moose", "tick", at));
         }        
     }
 }
