41150a7b34094eb8767fd0d1dde0c2dddefee41f
Neil Bourgeois <neil.bourgeois@gmail.com>
2015-01-04 15:46:21 -0700

    added controller for user scopes

---
+++ src/FWServices/Controllers/UserScopesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GF.FeatureWise.Services.Models;
+using GF.FeatureWise.Services.Repositories;
+
+namespace GF.FeatureWise.Services.Controllers
+{
+    public class UserScopesController : ApiController
+    {
+        private readonly IUserEventRepository repository;
+
+        public UserScopesController() : this(new UserEventRepository(new ApiDataContext()))
+        {
+        }
+
+        public UserScopesController(IUserEventRepository repository)
+        {
+            this.repository = repository;
+        }
+
+     
+        public HttpResponseMessage PostUserScope(UserScope userScope)
+        {
+            repository.Add(userScope.StartEvent());
+            var result = repository.Add(userScope.StopEvent());
+            var response = Request.CreateResponse(HttpStatusCode.Created, result);
+            var uri = Url.Link("DefaultApi", new { controller = "UserScopes", id = result.Id });
+            if (!String.IsNullOrEmpty(uri))
+            {
+                response.Headers.Location = new Uri(uri);
+            }
+            return response;
+        }
+    }
+}
---
+++ src/FWServices/Models/UserScope.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GF.FeatureWise.Services.Models
+{
+    public class UserScope
+    {
+        public Guid Id { get; set; }
+        public string Feature { get; set; }
+        public string Type { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+
+        public UserEvent StartEvent()
+        {
+            return new UserEvent {Id = Id, Type = "Start", Feature = Feature, At = Start};
+        }
+
+        public UserEvent StopEvent()
+        {
+            return new UserEvent { Id = Id, Type = "Stop", Feature = Feature, At = Stop };
+        }
+    }
+}
